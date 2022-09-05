8a5a90e47dda9dd9c24fc8c35c4a2d99a964a872
William Carroll <WilliamCarroll@ProtonMail.com>
2022-09-05 16:10:28 -0500

    Set up GUI within MonoRepo

---
+++ packages/gui/src/app.d.ts
@@ -0,0 +1,9 @@
+import {} from "sheet-handle";
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+declare namespace App {
+	// interface Locals {}
+	// interface PageData {}
+	// interface Platform {}
+}
