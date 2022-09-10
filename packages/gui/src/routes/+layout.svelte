93808cbca2ca666e66dc7ee87fa1eb9ea7978af1
William Carroll <WilliamCarroll@ProtonMail.com>
2022-09-10 13:05:59 -0500

    Add layout and styling

---
+++ packages/gui/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import "../style/general.scss";
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/import">Import</a>
+    <a href="/table">Table</a>
+</nav>
+
+<div class="slot-parent">
+    <slot />
+</div>
