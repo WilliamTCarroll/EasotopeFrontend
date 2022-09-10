07a35c5c251dc6b7f0febb9431b8040effafd3df
William Carroll <WilliamCarroll@ProtonMail.com>
2022-09-10 13:05:36 -0500

    Add functions to check the rows for errors

--- packages/gui/src/stores.ts
+++ packages/gui/src/stores.ts
@@ -1,9 +1,28 @@
 import { defaultConfig, ColumnConfig, Sample } from "sheet-handle";
 import { writable } from "svelte/store";
-import type { Writable } from "svelte/store";
+import type { Writable, Updater } from "svelte/store";
 
+/** Static String for the key of an input class */
+export const NOTE_CLASS = "__note_class__";
+/** Static String for the class that appears on an error field */
+export const ERR_CLASS = "error";
+/** Get the Column Configuration currently loaded */
 export function getColumnConfig(): ColumnConfig {
     return defaultConfig;
 }
 
 export let rows: Writable<Sample[]> = writable([]);
+
+export function checkRowsOk(data: Sample[]): boolean {
+    console.log("CHECK ROWS");
+    for (const s of data) {
+        for (const r of s.replicates) {
+            if (r.Disabled && r.Notes.trim().length === 0) {
+                console.log(r);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
